ddd87f9c1b6a77bf73f20df44a9bfe6bf4b37728
ekutsyi <1455087@stud.nau.edu.ua>
2022-10-01 10:03:01 +0300

    BitCounting 1st var

---
+++ BitCounting/BitCountingSrc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Kata
+{
+    public static int CountBits(int n)
+    {
+        int i = 0;
+        while (n != 0)
+        {
+            int bin = n % 2;
+            if (bin == 1)
+                i += 1;
+            n /= 2;
+        }
+        return i;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Kata.CountBits(11));
+    }
+}
