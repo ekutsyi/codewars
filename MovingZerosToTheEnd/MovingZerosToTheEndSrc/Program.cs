bc5334e91143fc3c60cc51ab3ef0a0236ea5c7c0
ekutsyi <1455087@stud.nau.edu.ua>
2022-10-14 21:14:54 +0300

    ok but

---
+++ MovingZerosToTheEnd/MovingZerosToTheEndSrc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Kata
+{
+    public static int[] MoveZeroes(int[] arr)
+    {
+        int i = 0;
+        int[] answer = new int[arr.Length];
+
+        foreach (int num in arr)
+        {
+            if (num != 0)
+                answer[i++] = num;
+        }
+
+        return answer;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {}
+}
