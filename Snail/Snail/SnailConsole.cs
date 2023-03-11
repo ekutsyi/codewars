d54d77ad469d8384eb6e736f3427c412a8603f83
ekutsyi <1455087@stud.nau.edu.ua>
2023-03-11 11:48:58 +0200

    Add Snail solution

---
+++ Snail/Snail/SnailConsole.cs
@@ -0,0 +1,33 @@
+﻿class HelloWorld
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        Console.Write("Enter number of rows in matrice: ");
+        var height = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter number of columns in matrice: ");
+        var length = Convert.ToInt32(Console.ReadLine());
+        int[,] input = new int[height, length];
+
+        Console.Write("Matrice with randomly generated values [0,100]:\n");
+        for (int i = 0; i < height; ++i)
+        {
+            for (int q = 0; q < length; ++q)
+            {
+                input[i, q] = rnd.Next(100);
+                Console.Write(input[i, q] + " ");
+            }
+            Console.Write("\n");
+        }
+
+        int[] output = SnailLibrary.Snail.doJob(input);
+        int ind = 0;
+
+        Console.Write("Snail array from matrice:\n");
+        while (ind < output.Length)
+        {
+            Console.Write(output[ind] + " ");
+            ++ind;
+        }
+    }
+}
